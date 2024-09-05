3199aea7237fe88791e22813902d3dcaa7f398c4
Ugyenjigmerangdrel <ugyen.jigme2018@gmail.com>
2024-09-05 16:48:32 +0530

    fixed the button and pricing tag sizing

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -10,15 +10,17 @@ export default function MealItem({
     <div className="meal-item">
       <article>
         <img src={itemImage} alt={itemName} />
-        <h3>{itemName}</h3>
-        <span className="meal-item-price ">${itemPrice}</span>
-        <p className="meal-item-description">{itemDescription}</p>
-        <button
-          onClick={() => onAdd(iIndex)}
-          className="button meal-item-actions"
-        >
-          Add to Cart
-        </button>
+        <div>
+          <h3>{itemName}</h3>
+          <p className="meal-item-price ">${itemPrice}</p>
+          <p className="meal-item-description">{itemDescription}</p>
+          <button
+            onClick={() => onAdd(iIndex)}
+            className="button meal-item-actions"
+          >
+            Add to Cart
+          </button>
+        </div>
       </article>
     </div>
   );
